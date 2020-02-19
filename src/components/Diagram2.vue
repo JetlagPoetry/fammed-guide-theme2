c279ae03ec3c9f4303851301702c26c701b4b8c0
Jingyuan <jingyuan_wang@outlook.com>
2020-02-19 18:57:29 -0500

    gojs advanced demo

--- src/components/Diagram2.vue
+++ src/components/Diagram2.vue
@@ -6,79 +6,175 @@
 <script>
 import go from 'gojs';
 export default {
-  name: "Diagram",
+  name: "Diagram2",
   data: () => ({
-
-    diagram: ''
+    diagram: '',
+    mt8: '',
+    mr8: '',
+    ml9: '',
+    roundedRectangleParams: '',
   }),
   props: ["modelData"],
   mounted: function() {
     let $ = go.GraphObject.make;
-          var self = this;
-          var myDiagram =
-            $(go.Diagram, this.$el,
-              {
-                layout: $(go.TreeLayout, { angle: 90, arrangement: go.TreeLayout.ArrangementHorizontal }),
-                "undoManager.isEnabled": true,
-                // Model ChangedEvents get passed up to component users
-                "ModelChanged": function(e) { self.$emit("model-changed", e); },
-                "ChangedSelection": function(e) { self.$emit("changed-selection", e); }
-              });
 
-          myDiagram.nodeTemplate =
-            $(go.Node, "Auto",
-              $(go.Shape,
+    this.mt8 = new go.Margin(8, 0, 0, 0);
+    this.mr8 = new go.Margin(0, 8, 0, 0);
+    this.ml8 = new go.Margin(0, 0, 0, 8);
+    this.roundedRectangleParams = {
+      parameter1: 2,  // set the rounded corner
+      spot1: go.Spot.TopLeft, spot2: go.Spot.BottomRight  // make content go all the way to inside edges of rounded corners
+    };
+    var myDiagram = $(go.Diagram, this.$el,  // the DIV HTML element
+          {
+            // Put the diagram contents at the top center of the viewport
+            initialDocumentSpot: go.Spot.LeftCenter,
+            initialViewportSpot: go.Spot.LeftCenter,
+            // OR: Scroll to show a particular node, once the layout has determined where that node is
+            // "InitialLayoutCompleted": function(e) {
+            //  var node = e.diagram.findNodeForKey(28);
+            //  if (node !== null) e.diagram.commandHandler.scrollToPart(node);
+            // },
+            layout:
+              $(go.TreeLayout,  // use a TreeLayout to position all of the nodes
                 {
-                  fill: "white", strokeWidth: 0,
-                  portId: "", fromLinkable: true, toLinkable: true, cursor: "pointer"
-                },
-                new go.Binding("fill", "color")),
-              $(go.TextBlock,
-                { margin: 8, editable: true },
-                new go.Binding("text").makeTwoWay())
-            );
+                  isOngoing: false,  // don't relayout when expanding/collapsing panels
+                  treeStyle: go.TreeLayout.StyleLastParents,
+                  // properties for most of the tree:
+                  angle: 0,
+                  layerSpacing: 80,
+                  // properties for the "last parents":
+                  alternateAngle: 0,
+                  alternateAlignment: go.TreeLayout.AlignmentStart,
+                  alternateNodeIndent: 15,
+                  alternateNodeIndentPastParent: 1,
+                  alternateNodeSpacing: 15,
+                  alternateLayerSpacing: 40,
+                  alternateLayerSpacingParentOverlap: 1,
+                  alternatePortSpot: new go.Spot(0.001, 1, 20, 0),
+                  alternateChildPortSpot: go.Spot.Left
+                })
+          });
 
-          myDiagram.linkTemplate =
-            $(go.Link,
-              { relinkableFrom: true, relinkableTo: true },
-              $(go.Shape),
-              $(go.Shape, { toArrow: "OpenTriangle" })
-            );
-
-          this.diagram = myDiagram;
-
-          this.updateModel(this.modelData);
-        },
-        watch: {
-          modelData: function(val) { this.updateModel(val); }
-        },
-        methods: {
-          model: function() { return this.diagram.model; },
-          updateModel: function(val) {
-            // No GoJS transaction permitted when replacing Diagram.model.
-            if (val instanceof go.Model) {
-              this.diagram.model = val;
-            } else {
-              var m = new go.GraphLinksModel()
-              if (val) {
-                for (var p in val) {
-                  m[p] = val[p];
-                }
-              }
-              this.diagram.model = m;
-            }
+    myDiagram.nodeTemplate =
+        $(go.Node, "Auto",
+          {
+            locationSpot: go.Spot.TopCenter,
+            isShadowed: true, shadowBlur: 1,
+            shadowOffset: new go.Point(0, 1),
+            shadowColor: "rgba(0, 0, 0, .14)",
+            selectionAdornmentTemplate:  // selection adornment to match shape of nodes
+              $(go.Adornment, "Auto",
+                $(go.Shape, "RoundedRectangle", this.roundedRectangleParams,
+                  { fill: null, stroke: "#7986cb", strokeWidth: 3 }
+                ),
+                $(go.Placeholder)
+              )  // end Adornment
           },
-          updateDiagramFromData: function() {
-            this.diagram.startTransaction();
-            // This is very general but very inefficient.
-            // It would be better to modify the diagramData data by calling
-            // Model.setDataProperty or Model.addNodeData, et al.
-            this.diagram.updateAllRelationshipsFromData();
-            this.diagram.updateAllTargetBindings();
-            this.diagram.commitTransaction("updated");
-          }
-        }
-      }
+          $(go.Shape, "RoundedRectangle", this.roundedRectangleParams,
+            { name: "SHAPE", fill: "#ffffff", strokeWidth: 0 },
+            // bluish if highlighted, white otherwise
+            new go.Binding("fill", "isHighlighted", function(h) { return h ? "#e8eaf6" : "#ffffff"; }).ofObject()
+          ),
+          $(go.Panel, "Vertical",
+            $(go.Panel, "Horizontal",
+              { margin: 8 },
+              $(go.Picture,  // flag image, only visible if a nation is specified
+                { margin: this.mr8, visible: false, desiredSize: new go.Size(50, 50) },
+                new go.Binding("source", "nation", this.theNationFlagConverter),
+                new go.Binding("visible", "nation", function(nat) { return nat !== undefined; }),
+              ),
+              $(go.Panel, "Table",
+                $(go.TextBlock,
+                  {
+                    row: 0, alignment: go.Spot.Left,
+                    font: "16px Roboto, sans-serif",
+                    stroke: "rgba(0, 0, 0, .87)",
+                    maxSize: new go.Size(160, NaN)
+                  },
+                  new go.Binding("text", "name")
+                ),
+                $(go.TextBlock, this.textStyle("title"),
+                  {
+                    row: 1, alignment: go.Spot.Left,
+                    maxSize: new go.Size(160, NaN)
+                  },
+                  new go.Binding("text", "title")
+                ),
+                $("PanelExpanderButton", "INFO",
+                  { row: 0, column: 1, rowSpan: 2, margin: this.ml8 }
+                )
+              ),
+            ),
+            $(go.Shape, "LineH",
+              {
+                stroke: "rgba(0, 0, 0, .60)", strokeWidth: 1,
+                height: 1, stretch: go.GraphObject.Horizontal
+              },
+              new go.Binding("visible").ofObject("INFO")  // only visible when info is expanded
+            ),
+            $(go.Panel, "Vertical",
+              {
+                name: "INFO",  // identify to the PanelExpanderButton
+                stretch: go.GraphObject.Horizontal,  // take up whole available width
+                margin: 8,
+                defaultAlignment: go.Spot.Left,  // thus no need to specify alignment on each element
+              },
+              $(go.TextBlock, this.textStyle("headOf"),
+                new go.Binding("text", "headOf", function(head) { return "Head of: " + head; })
+              ),
+              $(go.TextBlock, this.textStyle("boss"),
+                new go.Binding("margin", "headOf", function() { return this.mt8; }), // some space above if there is also a headOf value
+                new go.Binding("text", "boss", function(boss) {
+                  var boss2 = myDiagram.model.findNodeDataForKey(boss);
+                  if (boss2 !== null) {
+                    return "Reporting to: " + boss2.name;
+                  }
+                  return "";
+                })
+              )
+            )
+          )
+        );
+
+    myDiagram.linkTemplate =
+        $(go.Link, go.Link.Orthogonal,
+          { corner: 5, selectable: false },
+          $(go.Shape, { strokeWidth: 3, stroke: "#424242" }));  // dark gray, rounded corner links
+
+    myDiagram.model =
+      $(go.TreeModel,
+        {
+          nodeParentKeyProperty: "boss",  // this property refers to the parent node data
+          nodeDataArray: this.modelData,
+        });
+
+    this.diagram = myDiagram;
+
+    // this.updateModel(this.modelData);
+  },
+  watch: {
+    modelData: function(val) { this.updateModel(val); }
+  },
+  methods: {
+    model: function() { return this.diagram.model; },
+
+    textStyle(field) {
+      return [
+        {
+          font: "12px Roboto, sans-serif", stroke: "rgba(0, 0, 0, .60)",
+          visible: false  // only show textblocks when there is corresponding data for them
+        },
+        new go.Binding("visible", field, function(val) { return val !== undefined; })
+      ];
+    },
+
+      // define Converters to be used for Bindings
+    theNationFlagConverter(nation) {
+      return "https://www.nwoods.com/images/emojiflags/" + nation + ".png";
+    }
+  }
+}
 </script>
 
 <style scoped>
--- src/components/Summary.vue
+++ src/components/Summary.vue
@@ -7,7 +7,7 @@
 <!--         <org-chart :datasource="ds">
         </org-chart> -->
         <div id="sample">
-    <diagram2 ref="diag" v-bind:model-data="diagramData" v-on:model-changed="modelChanged" v-on:changed-selection="changedSelection" style="border: solid 1px black; width:100%; height:400px"></diagram2>
+    <diagram2 ref="diag" v-bind:model-data="nodeDataArray" style="background-color: #f2f2f2; border: solid 1px black; width: 100%; height: 700px"></diagram2>
     <v-btn @click="addNode()">Add Child to Gamma</v-btn>
     <v-btn @click="modifyStuff()">Modify view model data without undo</v-btn>
     <br>Current Node:
@@ -43,7 +43,6 @@
 <script>
 // import OrgChart from './OrganizationChartContainer.vue'
 import Diagram2 from './Diagram2.vue'
-import go from 'gojs';
 export default {
   name: 'Summary',
 
@@ -53,102 +52,54 @@ export default {
   },
 
   data: () => ({
-  diagramData: {  // passed to  as its modelData
-        nodeDataArray: [
-          { key: 1, text: "Alpha", color: "lightblue" },
-          { key: 2, text: "Beta", color: "orange" },
-          { key: 3, text: "Gamma", color: "lightgreen" },
-          { key: 4, text: "Delta", color: "pink" }
-        ],
-        linkDataArray: [
-          { from: 1, to: 2 },
-          { from: 1, to: 3 },
-          { from: 3, to: 4 }
-        ]
-      },
-      currentNode: null,
-      savedModelText: "",
-      counter: 1,  // used by addNode
-      counter2: 4,  // used by modifyStuff
+  nodeDataArray : [
+        { key: 0, name: "Ban Ki-moon 반기문", nation: "SouthKorea", title: "Secretary-General of the United Nations", headOf: "Secretariat" },
+        { key: 1, boss: 0, name: "Patricia O'Brien", nation: "Ireland", title: "Under-Secretary-General for Legal Affairs and United Nations Legal Counsel", headOf: "Office of Legal Affairs" },
+        { key: 3, boss: 1, name: "Peter Taksøe-Jensen", nation: "Denmark", title: "Assistant Secretary-General for Legal Affairs" },
+        { key: 9, boss: 3, name: "Other Employees" },
+        { key: 4, boss: 1, name: "Maria R. Vicien - Milburn", nation: "Argentina", title: "General Legal Division Director", headOf: "General Legal Division" },
+        { key: 10, boss: 4, name: "Other Employees" },
+        { key: 5, boss: 1, name: "Václav Mikulka", nation: "CzechRepublic", title: "Codification Division Director", headOf: "Codification Division" },
+        { key: 11, boss: 5, name: "Other Employees" },
+        { key: 6, boss: 1, name: "Sergei Tarassenko", nation: "Russia", title: "Division for Ocean Affairs and the Law of the Sea Director", headOf: "Division for Ocean Affairs and the Law of the Sea" },
+        { key: 12, boss: 6, name: "Alexandre Tagore Medeiros de Albuquerque", nation: "Brazil", title: "Chairman of the Commission on the Limits of the Continental Shelf", headOf: "The Commission on the Limits of the Continental Shelf" },
+        { key: 17, boss: 12, name: "Peter F. Croker", nation: "Ireland", title: "Chairman of the Committee on Confidentiality", headOf: "The Committee on Confidentiality" },
+        { key: 31, boss: 17, name: "Michael Anselme Marc Rosette", nation: "Seychelles", title: "Vice Chairman of the Committee on Confidentiality" },
+        { key: 32, boss: 17, name: "Kensaku Tamaki", nation: "Japan", title: "Vice Chairman of the Committee on Confidentiality" },
+        { key: 33, boss: 17, name: "Osvaldo Pedro Astiz", nation: "Argentina", title: "Member of the Committee on Confidentiality" },
+        { key: 34, boss: 17, name: "Yuri Borisovitch Kazmin", nation: "Russia", title: "Member of the Committee on Confidentiality" },
+        { key: 18, boss: 12, name: "Philip Alexander Symonds", nation: "Australia", title: "Chairman of the Committee on provision of scientific and technical advice to coastal States", headOf: "Committee on provision of scientific and technical advice to coastal States" },
+        { key: 35, boss: 18, name: "Emmanuel Kalngui", nation: "Cameroon", title: "Vice Chairman of the Committee on provision of scientific and technical advice to coastal States" },
+        { key: 36, boss: 18, name: "Sivaramakrishnan Rajan", nation: "India", title: "Vice Chairman of the Committee on provision of scientific and technical advice to coastal States" },
+        { key: 37, boss: 18, name: "Francis L. Charles", nation: "TrinidadAndTobago", title: "Member of the Committee on provision of scientific and technical advice to costal States" },
+        { key: 38, boss: 18, name: "Mihai Silviu German", nation: "Romania", title: "Member of the Committee on provision of scientific and technical advice to costal States" },
+        { key: 19, boss: 12, name: "Lawrence Folajimi Awosika", nation: "Nigeria", title: "Vice Chairman of the Commission on the Limits of the Continental Shelf" },
+        { key: 20, boss: 12, name: "Harald Brekke", nation: "Norway", title: "Vice Chairman of the Commission on the Limits of the Continental Shelf" },
+        { key: 21, boss: 12, name: "Yong-Ahn Park", nation: "SouthKorea", title: "Vice Chairman of the Commission on the Limits of the Continental Shelf" },
+        { key: 22, boss: 12, name: "Abu Bakar Jaafar", nation: "Malaysia", title: "Chairman of the Editorial Committee", headOf: "Editorial Committee" },
+        { key: 23, boss: 12, name: "Galo Carrera Hurtado", nation: "Mexico", title: "Chairman of the Training Committee", headOf: "Training Committee" },
+        { key: 24, boss: 12, name: "Indurlall Fagoonee", nation: "Mauritius", title: "Member of the Commission on the Limits of the Continental Shelf" },
+        { key: 25, boss: 12, name: "George Jaoshvili", nation: "Georgia", title: "Member of the Commission on the Limits of the Continental Shelf" },
+        { key: 26, boss: 12, name: "Wenzhang Lu", nation: "China", title: "Member of the Commission on the Limits of the Continental Shelf" },
+        { key: 27, boss: 12, name: "Isaac Owusu Orudo", nation: "Ghana", title: "Member of the Commission on the Limits of the Continental Shelf" },
+        { key: 28, boss: 12, name: "Fernando Manuel Maia Pimentel", nation: "Portugal", title: "Member of the Commission on the Limits of the Continental Shelf" },
+        { key: 7, boss: 1, name: "Renaud Sorieul", nation: "France", title: "International Trade Law Division Director", headOf: "International Trade Law Division" },
+        { key: 13, boss: 7, name: "Other Employees" },
+        { key: 8, boss: 1, name: "Annebeth Rosenboom", nation: "Netherlands", title: "Treaty Section Chief", headOf: "Treaty Section" },
+        { key: 14, boss: 8, name: "Bradford Smith", nation: "UnitedStates", title: "Substantive Legal Issues Head", headOf: "Substantive Legal Issues" },
+        { key: 29, boss: 14, name: "Other Employees" },
+        { key: 15, boss: 8, name: "Andrei Kolomoets", nation: "Russia", title: "Technical/Legal Issues Head", headOf: "Technical/Legal Issues" },
+        { key: 30, boss: 15, name: "Other Employees" },
+        { key: 16, boss: 8, name: "Other Employees" },
+        { key: 2, boss: 0, name: "Heads of Other Offices/Departments" }
+      ],
     ds :{ "id": "1", "name": "1. CONSTITUER UN GROUPE DE TRAVAIL EN RPO ET ÉTABLIR COLLECTIVEMENT DES PROCESSUS DE FONCTIONNEMENT. ", "children": [ { "id": "2", "name": "1.1. RECRUTER DES REPRÉSENTANTS DES PARTIES PRENANTES QUI ONT UNE EXPÉRIENCE DE TRAVAIL EN GROUPE. ", "title": "", "selected": true }, { "id": "3", "name": "1.2. RECRUTER DES MEMBRES DU GROUPE DE TRAVAIL QUI PROVIENNENT DE TOUTES LES PARTIES PRENANTES, DONT LA GESTION.", "title": "asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf", "selected": false }, { "id": "4", "name": "1.3. ÉVALUER ET SATISFAIRE AUX BESOINS EN FORMATION DES MEMBRES DU GROUPE DE TRAVAIL.", "title": "", "selected": true }, { "id": "5", "name": "1.4. ÉTABLIR DES PROCESSUS DE GESTION DE PROJET.", "title": "", "selected": true }, { "id": "6", "name": "1.5. PLANIFIER ET TENIR DES RÉUNIONS À INTERVALLES RÉGULIERS.", "title": "", "selected": true }, { "id": "7", "name": "1.6. S’ASSURER QUE LES RÉUNIONS SONT STRUCTURÉES, CIBLÉES ET ÉVALUÉES.", "title": "", "selected": true }, { "id": "8", "name": "1.7. S’ENTENDRE SUR DES MÉCANISMES DE COMMUNICATIONS.", "title": "", "selected": true }, { "id": "9", "name": "1.8. ÉTABLIR DES MÉCANISMES ASSURANT LA CONTINUITÉ.", "title": "", "selected": true } ] }
   }),
 
   computed: {
-          currentNodeText: {
-            get: function() {
-              var node = this.currentNode;
-              if (node instanceof go.Node) {
-                return node.data.text;
-              } else {
-                return "";
-              }
-            },
-            set: function(val) {
-              var node = this.currentNode;
-              if (node instanceof go.Node) {
-                var model = this.model();
-                model.startTransaction();
-                model.setDataProperty(node.data, "text", val);
-                model.commitTransaction("edited text");
-              }
-            }
-          }
-        },
+    
+  },
   methods: {
-    // get access to the GoJS Model of the GoJS Diagram
-    model: function() { return this.$refs.diag.model(); },
-
-    // tell the GoJS Diagram to update based on the arbitrarily modified model data
-    updateDiagramFromData: function() { this.$refs.diag.updateDiagramFromData(); },
-
-    // this event listener is declared on the 
-    modelChanged: function(e) {
-      if (e.isTransactionFinished) {  // show the model data in the page's TextArea
-        this.savedModelText = e.model.toJson();
-      }
-    },
-
-    changedSelection: function(e) {
-      var node = e.diagram.selection.first();
-      if (node instanceof go.Node) {
-        this.currentNode = node;
-        this.currentNodeText = node.data.text;
-      } else {
-        this.currentNode = null;
-        this.currentNodeText = "";
-      }
-    },
-
-    // Here we modify the GoJS Diagram's Model using its methods,
-    // which can be much more efficient than modifying some memory and asking
-    // the GoJS Diagram to find differences and update accordingly.
-    // Undo and Redo will work as expected.
-    addNode: function() {
-      var model = this.model();
-      model.startTransaction();
-      model.setDataProperty(model.findNodeDataForKey(4), "color", "purple");
-      var data = { text: "NEW " + this.counter++, color: "yellow" };
-      model.addNodeData(data);
-      model.addLinkData({ from: 3, to: model.getKeyForNodeData(data) });
-      model.commitTransaction("added Node and Link");
-      // also manipulate the Diagram by changing its Diagram.selection collection
-      var diagram = this.$refs.diag.diagram;
-      diagram.select(diagram.findNodeForData(data));
-    },
-
-    // Here we modify VUE's view model directly, and
-    // then ask the GoJS Diagram to update everything from the data.
-    // This is less efficient than calling the appropriate GoJS Model methods.
-    // NOTE: Undo will not be able to restore all of the state properly!!
-    modifyStuff: function() {
-      var data = this.diagramData;
-      data.nodeDataArray[0].color = "red";
-      // Note here that because we do not have the GoJS Model,
-      // we cannot find out what values would be unique keys, for reference by the link data.
-      data.nodeDataArray.push({ key: ++this.counter2, text: this.counter2.toString(), color: "orange" });
-      data.linkDataArray.push({ from: 2, to: this.counter2 });
-      this.updateDiagramFromData();
-    }
   }
 };
 </script>
